ec14c51db99ca04bca990322d994e6bcfa9330d6
W Y Lim <wan208@naver.com>
2020-11-17 16:14:34 +0900

    introduce re

--- src/crypto/Introduce.java
+++ src/crypto/Introduce.java
@@ -8,7 +8,7 @@ public class Introduce {
         String my_name = "홍길동";
         int my_age = 25;
        
-        System.out.printf("%s 제 이름은 ""%s""이고 나이는 %s살 입니다.\n", 인삿말, my_name, my_age);
+        System.out.printf("%s 제 이름은 %s이고 나이는 %s살 입니다.\n", 인삿말, my_name, my_age);
        
         return;
     }   
